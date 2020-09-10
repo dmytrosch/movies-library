3f63c10c355a3d5b992f93fda3ba19623672e876
Dmytro.Sch <dmytro.sch@list.ru>
2020-09-10 16:11:43 +0300

    replace some fetch vars

--- src/js/Api/fetchMethods.js
+++ src/js/Api/fetchMethods.js
@@ -1,37 +1,40 @@
-import {pageNumber} from '../components/globalVars.js'
+import { pageNumber } from '../components/globalVars.js';
 
-const baseUrl = 'https://api.themoviedb.org/3'
-const movieSearch = '/search/movie'
-const API_KEY = '?api_key=0c84539b7b6fe9bdba856aa5f27d88e0'
+const baseUrl = 'https://api.themoviedb.org/3';
+const API_KEY = '?api_key=0c84539b7b6fe9bdba856aa5f27d88e0';
 
-export default { 
-
-    query: '', 
-    fetchMovieSearch(){
-        const pageCount = `&page=${pageNumber}`
-        const search = `&query=${this.query}`
-        return fetch(baseUrl+movieSearch+API_KEY+search+pageCount).then(response => response.json()).then(parcedData => {
-            this.incrementPage();
-            return parcedData.results;
-        })
+export default {
+    query: '',
+    fetchMovieSearch() {
+        const pageCount = `&page=${pageNumber}`;
+        const movieSearch = '/search/movie';
+        const searchQuery = `&query=${this.query}`;
+        return fetch(baseUrl + movieSearch + API_KEY + searchQuery + pageCount)
+            .then(response => response.json())
+            .then(parcedData => {
+                this.incrementPage();
+                return parcedData.results;
+            });
     },
-    get searchQuery(){
+    get searchQuery() {
         return this.query;
     },
-    set searchQuery(string){
+    set searchQuery(string) {
         this.query = string;
-    }, 
-    incrementPage(){
+    },
+    incrementPage() {
         pageNumber += 1;
-    }, 
-    decrementPage(){
-        pageNumber --;
-    }, 
-    fetchIdSearch(id){
-        const idSearch = '/movie/';
-        return fetch(baseUrl+idSearch+id+API_KEY).then(response => response.json()).then(parcedMovieById => {
-            this.incrementPage();
-            return parcedMovieById;
-        })
-    }
-}
+    },
+    decrementPage() {
+        pageNumber--;
+    },
+    fetchIdSearch(id) {
+        const idSearch = `/movie/${id}`;
+        return fetch(baseUrl + idSearch + API_KEY)
+            .then(response => response.json())
+            .then(parcedMovieById => {
+                this.incrementPage();
+                return parcedMovieById;
+            });
+    },
+};
