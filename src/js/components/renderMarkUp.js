f3448a73072ba23fe8907946e3ae0f3cefd96cc4
Slavik Ohorodnik <teatrgsc@gmail.com>
2020-09-11 18:54:20 +0300

    -d function at renderMarkup, index.js

--- src/js/components/renderMarkUp.js
+++ src/js/components/renderMarkUp.js
@@ -2,12 +2,6 @@ import movieSearch from '../Api/fetchMethods'
 import filmCardListTemplate from '../../templates/filmCardsListTemplate.hbs';
 import filmPageTemplate from '../../templates/filmPageTemplate.hbs';
 
-const galleryRef = document.querySelector('.films-library__gallery')
 export default {
-  async temp() {
-    const query = 'грусть';
-    const data = await movieSearch.movieSearch(query);
-    const mupkup = filmPageTemplate(data);
-    galleryRef.insertAdjacentHTML('beforeend', mupkup)
-  }
+  
 }
