183bb430561dd91abca475f8af8ac4e8ab56e902
Volodymyr Nechyporenko <vol.nechyporenko@gmail.com>
2020-09-11 20:07:48 +0300

    add film page

--- src/js/components/renderMarkUp.js
+++ src/js/components/renderMarkUp.js
@@ -1,6 +1,17 @@
+import fetchMethods from '../Api/fetchMethods';
 import filmCardListTemplate from '../../templates/filmCardsListTemplate.hbs';
-import filmPageTemplate from '../../templates/filmPageTemplate.hbs'
+import filmPageTemplate from '../../templates/filmPageTemplate.hbs';
+const filmListResults = [];
 
 export default {
-    
-}
+    filmPage = async (id) => {
+        const filmInList = filmListResults.find(item => item.id === id);
+        const selectedFilm = filmInList
+        ? filmInList
+        : await fetchMethods.idSearch(id);
+        filmPageTemplate(selectedFilm);
+        return selectedFilm;
+    },
+};
+
+
