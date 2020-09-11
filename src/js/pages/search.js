c75c584c8d085c96cecf5b10fe935cfe0c25f00e
anatolii.parubets <anatolii.parubets@betlab.com>
2020-09-11 21:28:46 +0300

    fix: fix pagination

--- src/js/pages/search.js
+++ src/js/pages/search.js
@@ -2,8 +2,6 @@ import fetch from '../Api/fetchMethods';
 import { pageNumber } from '../components/globalVars.js';
 import globalVars from '../components/globalVars.js'
 
-
-
 const refs = {
     inputForm: document.querySelector('#search-form'),
     prevBtn: document.querySelector('button[data-action="decrement"]'),
@@ -15,38 +13,49 @@ refs.inputForm.addEventListener('submit', searchFormHandler);
 refs.prevBtn.addEventListener('click', paginationPrevBtnHandler)
 refs.nextBtn.addEventListener('click', paginationNextBtnHandler)
 
+refs.prevBtn.disabled = true;
+
 async function searchFormHandler(event) {
     event.preventDefault();
     globalVars.searchQuery = event.target.elements.query.value;
     let fetchResult;
+
     if (globalVars.searchQuery) {
         globalVars.resetPage()
         fetchResult = await fetch.movieSearch(globalVars.searchQuery);
     }
-    if(fetchResult.length === 0){
+
+    if (fetchResult.length === 0) {
         // pnotify / alert
-    }else{
+    } else {
         //render списка
     }
     console.log(fetchResult);
     console.log(pageNumber);
-    
-}
-refs.prevBtn.disabled = true;
 
-function paginationPrevBtnHandler(){
-    globalVars.decrementPage() 
-    refs.span.textContent = pageNumber; 
+    refs.span.textContent = pageNumber;
+}
+
+async function paginationPrevBtnHandler(){
+    globalVars.decrementPage()
+    refs.span.textContent = pageNumber;
+
+    const data = await fetch.movieSearch(globalVars.searchQuery);
+
+    console.log(data)
+
     if (pageNumber === 1){
         refs.prevBtn.disabled = true;
-    }      
+    }
 }
 
-function paginationNextBtnHandler(){
-
+async function paginationNextBtnHandler(){
     globalVars.incrementPage()
     refs.prevBtn.disabled = false;
-    fetch.movieSearch(globalVars.searchQuery).then(data => data)
+
+    const data = await fetch.movieSearch(globalVars.searchQuery);
+
+    console.log(data)
+
     refs.span.textContent = pageNumber;
-    
 }
