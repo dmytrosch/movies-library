1a7ecb342c3b5078e552fc77dc146c435fc0c28f
Dmytro.Sch <dmytro.sch@list.ru>
2020-09-11 22:44:27 +0300

    edit global vars file and seperate pagination functions to file

--- src/js/components/globalVars.js
+++ src/js/components/globalVars.js
@@ -1,19 +1,12 @@
-export let selectFilm;
-export let pageNumber = 1;
-export let filmListResult;
+// export let selectFilm;
+// export let pageNumber = 1;
+// export let filmListResult;
 // export let searchQuery = '';
 
+// 
 export default {
-    incrementPage() {
-        pageNumber ++;
-    },
-    decrementPage() {
-        pageNumber --;
-    },
-    resetPage() {
-        pageNumber = 1;
-    },
-    searchQuery : '',
- }
-
+    pageNumber: 1,
+    filmListResult: null,
+    searchQuery: null,
+}
 
