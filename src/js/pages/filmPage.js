abad8bb06dd69e5bea251e77f7b03b9e8cc5786a
Volodymyr Nechyporenko <vol.nechyporenko@gmail.com>
2020-09-12 15:50:27 +0300

    edit add to watched end add to queue in film page

--- src/js/pages/filmPage.js
+++ src/js/pages/filmPage.js
@@ -12,6 +12,9 @@ export default async function filmPage(id) {
     const checkIsInList = list =>
         list.find(item => item.id === selectedFilm.id);
 
+    // Обрабатываем состояние кнопок после рендера
+    monitorButtonStatusText();
+
     // Записываем данные в LocalStorage.
     function setToLS(key, data) {
         localStorage.setItem(key, JSON.stringify(data));
@@ -26,32 +29,68 @@ export default async function filmPage(id) {
     function monitorButtonStatusText() {
         const watchedFilms = getFromLS(WATCHED_KEY_IN_LS);
         const queueFilms = getFromLS(QUEUE_KEY_IN_LS);
-        queueBtn.innerHTML = checkIsInList(queueFilms)
-            ? 'Delete from queue'
-            : 'Add to queue';
-        watchedBtn.innerHTML = checkIsInList(watchedFilms)
-            ? 'Delete from watched'
-            : 'Add to watched';
+        const isWatched = checkIsInList(watchedFilms);
+        const isInQueue = checkIsInList(queueFilms);
+        if (isWatched) {
+            watchedBtn.innerHTML = 'Delete from watched';
+            queueBtn.innerHTML = 'Watch again';
+        }
+        if (isInQueue) {
+            watchedBtn.innerHTML = 'Add to watched';
+            queueBtn.innerHTML = 'Delete from queue';
+        }
+        if (!isWatched && !isInQueue) {
+            queueBtn.innerHTML = 'Add to queue';
+            watchedBtn.innerHTML = 'Add to watched';
+        }
     }
 
     // Добавляем или удаляем фильм из списка "Очереди"
     function toggleToQueue() {
         const queueFilms = getFromLS(QUEUE_KEY_IN_LS);
-        setToLS(QUEUE_KEY_IN_LS, getNewFilmList(queueFilms, selectedFilm));
+        const watchedFilms = getFromLS(WATCHED_KEY_IN_LS);
+        const isInQueue = checkIsInList(queueFilms);
+
+        if (isInQueue) {
+            deleteFromList(queueFilms, QUEUE_KEY_IN_LS);
+        } else {
+            // Если фильма не было в очереди просмотра, то добавляем в очереди просмотра и удаляем из просмотренных
+            addToList(queueFilms, QUEUE_KEY_IN_LS);
+            deleteFromList(watchedFilms, WATCHED_KEY_IN_LS);
+        }
+
         monitorButtonStatusText();
     }
 
     // Добавляем или удаляем фильм из списка "просмотренных" фильмов.
     function toggleToWatched() {
         const watchedFilms = getFromLS(WATCHED_KEY_IN_LS);
-        setToLS(WATCHED_KEY_IN_LS, getNewFilmList(watchedFilms, selectedFilm));
+        const queueFilms = getFromLS(QUEUE_KEY_IN_LS);
+        const isWatched = checkIsInList(watchedFilms);
+        if (isWatched) {
+            // Если фильм был в списке просмотренных удаляем его оттуда
+            deleteFromList(watchedFilms, WATCHED_KEY_IN_LS);
+        } else {
+            // Если фильма не было в просмотренных, то добавляем в просмотренные и удаляем из очереди просмотра
+            addToList(watchedFilms, WATCHED_KEY_IN_LS);
+            deleteFromList(queueFilms, QUEUE_KEY_IN_LS);
+        }
+
         monitorButtonStatusText();
     }
 
-    // Получаем новый список фильмов в зависимости от того есть этот фильм в списке или нет.
-    function getNewFilmList(list) {
-        return checkIsInList(list)
-            ? list.filter(item => item.id !== selectedFilm.id)
-            : list.concat([selectedFilm]);
+    // Удаляет фильм из списка, если он там был
+    function deleteFromList(list, key) {
+        if (checkIsInList(list)) {
+            setToLS(
+                key,
+                list.filter(item => item.id !== selectedFilm.id),
+            );
+        }
+    }
+
+    // Добавляем фильм в список
+    function addToList(list, key) {
+        setToLS(key, list.concat([selectedFilm]));
     }
 }
