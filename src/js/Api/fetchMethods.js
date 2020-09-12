5a205164d510de13fa6609d273205af97b20da01
Oleksandr Zdebskyi <technomaster@ukr.net>
2020-09-12 20:50:16 +0300

    added try/catch , added Not found template

--- src/js/Api/fetchMethods.js
+++ src/js/Api/fetchMethods.js
@@ -2,7 +2,7 @@ import globalVars from '../components/globalVars';
 
 const baseUrl = 'https://api.themoviedb.org/3';
 const API_KEY = '?api_key=0c84539b7b6fe9bdba856aa5f27d88e0';
-const queryLanguage = '&language=en-US'
+const queryLanguage = '&language=en-US';
 
 export default {
     movieSearch(query) {
@@ -10,12 +10,18 @@ export default {
         const movieSearch = '/search/movie';
         const searchQueryString = `&query=${query}`;
         return fetch(
-            baseUrl + movieSearch + API_KEY + queryLanguage + searchQueryString + pageCount)
-        
+            baseUrl +
+                movieSearch +
+                API_KEY +
+                queryLanguage +
+                searchQueryString +
+                pageCount,
+        )
             .then(response => response.json())
             .then(parcedData => {
                 return parcedData.results;
-            });
+            })
+            .catch(error => console.log(error));
     },
 
     idSearch(id) {
@@ -26,11 +32,11 @@ export default {
                 return parcedMovieById;
             });
     },
-    popularSearch(){
-        const popularUrl = '/movie/popular'
-        return fetch(baseUrl + popularUrl + API_KEY+ queryLanguage + globalVars.pageNumber).then(response => response.json())
-    }
+    popularSearch() {
+        const popularUrl = '/movie/popular';
+        const pageCount = `&page=${globalVars.pageNumber}`;
+        return fetch(baseUrl + popularUrl + API_KEY + queryLanguage + pageCount)
+            .then(response => response.json())
+            .catch(error => console.log(error));
+    },
 };
-
-
-
--- src/js/pages/search.js
+++ src/js/pages/search.js
@@ -1,12 +1,14 @@
 import fetch from '../Api/fetchMethods';
 import globalVars from '../components/globalVars.js';
 import pagination from '../components/pagination';
+import epmtyRequest from '../../templates/emptyRequestTemplate.hbs';
 
 const refs = {
     inputForm: document.querySelector('#search-form'),
     prevBtn: document.querySelector('button[data-action="decrement"]'),
     nextBtn: document.querySelector('button[data-action="increment"]'),
     span: document.querySelector('.page-number'),
+    list: document.querySelector('.films-library__gallery'),
 };
 let fetchResult;
 
@@ -17,57 +19,75 @@ refs.nextBtn.addEventListener('click', paginationNextBtnHandler);
 disableBtn(refs.prevBtn);
 
 async function searchFormHandler(event) {
-    event.preventDefault();
-    globalVars.searchQuery = event.target.elements.query.value;
-    if (globalVars.searchQuery) {
-        pagination.resetPage();
-        fetchResult = await fetch.movieSearch(globalVars.searchQuery);
-    }
+    try {
+        event.preventDefault();
+        globalVars.searchQuery = event.target.elements.query.value;
+        if (globalVars.searchQuery) {
+            pagination.resetPage();
+            fetchResult = await fetch.movieSearch(globalVars.searchQuery);
+        }
 
-    if (fetchResult.length === 0) {
-        // pnotify / alert
-    } else {
-        //render списка
-    }
-    console.log(fetchResult);
-    console.log(globalVars.pageNumber, 'before func');
-    await checkNextPageResult();
-    console.log(globalVars.pageNumber, 'after func');
+        if (fetchResult.length === 0) {
+            // pnotify / alert
+            const markup = epmtyRequest(globalVars.searchQuery);
+            refs.list.insertAdjacentHTML('beforeend', markup);
+        } else {
+            //render списка
+        }
+        console.log(fetchResult);
+        console.log(globalVars.pageNumber, 'before func');
+        await checkNextPageResult();
+        console.log(globalVars.pageNumber, 'after func');
 
-    refs.span.textContent = globalVars.pageNumber;
+        refs.span.textContent = globalVars.pageNumber;
+    } catch (error) {
+        throw error;
+    }
 }
 
 async function paginationPrevBtnHandler() {
-    pagination.decrementPage();
-    refs.span.textContent = globalVars.pageNumber;
+    try {
+        pagination.decrementPage();
+        refs.span.textContent = globalVars.pageNumber;
 
-    fetchResult = await fetch.movieSearch(globalVars.searchQuery);
+        fetchResult = await fetch.movieSearch(globalVars.searchQuery);
 
-    console.log(fetchResult);
+        console.log(fetchResult);
 
-    if (globalVars.pageNumber === 1) {
-        disableBtn(refs.prevBtn);
+        if (globalVars.pageNumber === 1) {
+            disableBtn(refs.prevBtn);
+        }
+    } catch (error) {
+        throw error;
     }
 }
 
 async function paginationNextBtnHandler() {
-    pagination.incrementPage();
-    refs.prevBtn.disabled = false;
+    try {
+        pagination.incrementPage();
+        refs.prevBtn.disabled = false;
 
-    fetchResult = await fetch.movieSearch(globalVars.searchQuery);
-    await checkNextPageResult();
-    console.log(fetchResult);
+        fetchResult = await fetch.movieSearch(globalVars.searchQuery);
+        await checkNextPageResult();
+        console.log(fetchResult);
 
-    refs.span.textContent = globalVars.pageNumber;
+        refs.span.textContent = globalVars.pageNumber;
+    } catch (error) {
+        throw error;
+    }
 }
 
 async function checkNextPageResult() {
-    globalVars.pageNumber++;
-    const nextFetchResult = await fetch.movieSearch(globalVars.searchQuery);
-    globalVars.pageNumber--;
-    console.log('func', globalVars.pageNumber, nextFetchResult);
-    if (nextFetchResult.length === 0) {
-        disableBtn(refs.nextBtn);
+    try {
+        globalVars.pageNumber++;
+        const nextFetchResult = await fetch.movieSearch(globalVars.searchQuery);
+        globalVars.pageNumber--;
+        console.log('func', globalVars.pageNumber, nextFetchResult);
+        if (nextFetchResult.length === 0) {
+            disableBtn(refs.nextBtn);
+        }
+    } catch (error) {
+        throw error;
     }
 }
 
@@ -76,9 +96,11 @@ function disableBtn(elementBtn) {
     elementBtn.disabled = true;
 }
 
-async function showPopularMovies (){
-    const popularMoviesResult = await fetch.popularSearch()
-    console.log(popularMoviesResult.results);
+async function showPopularMovies() {
+    try {
+        const popularMoviesResult = await fetch.popularSearch();
+        console.log('showPopularMovies', popularMoviesResult.results);
+    } catch (error) {
+        throw error;
+    }
 }
-
-showPopularMovies ()
