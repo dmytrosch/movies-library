6698e7ffe1b886680b6a8381675120a15a6600c0
Yuriy Tymchyna <ytymchyna@gmail.com>
2020-09-12 16:18:23 +0300

    add mainpage template in templates directory, change filmCardsTemplate file, make a call of mainPage function in index.js and add add a code into mainPage file in pages directory

--- src/index.js
+++ src/index.js
@@ -20,32 +20,35 @@ import notFoundPage from './js/pages/404';
 import './js/pages/search.js';
 import renderMarkUp from './js/components/renderMarkUp';
 
+mainPage();
+
 window['router'] = new Router({
-  root: '/',
-  routes: [{
-      path: /film\/(.*)/,
-      callback: id => {
-        filmPage(id);
-      },
+    root: '/',
+    routes: [
+        {
+            path: /film\/(.*)/,
+            callback: id => {
+                filmPage(id);
+            },
+        },
+        {
+            path: '',
+            callback: () => {
+                mainPage();
+            },
+        },
+    ],
+    error: {
+        callback: () => {
+            notFoundPage();
+        },
     },
-    {
-      path: '',
-      callback: () => {
-        mainPage();
-      },
-    },
-  ],
-  error: {
-    callback: () => {
-      notFoundPage();
-    },
-  },
 });
 
 const refs = {
-  headerLogo: document.querySelector('.library__nav-icon'),
-  headerLinkHome: document.querySelector('#headerNavHome'),
-  headerLinkMyLibrary: document.querySelector('#headerNavMyLibrary'),
+    headerLogo: document.querySelector('.library__nav-icon'),
+    headerLinkHome: document.querySelector('#headerNavHome'),
+    headerLinkMyLibrary: document.querySelector('#headerNavMyLibrary'),
 };
 
 refs.headerLogo.addEventListener('click', onMainPageClickHandler);
@@ -53,12 +56,12 @@ refs.headerLinkHome.addEventListener('click', onMainPageClickHandler);
 refs.headerLinkMyLibrary.addEventListener('click', onLibraryBtnHandler);
 
 function onMainPageClickHandler(event) {
-  event.target.preventDefault();
-  mainPage();
+    event.target.preventDefault();
+    mainPage();
 }
 
 function onLibraryBtnHandler(event) {
-  event.target.preventDefault();
-  library();
+    event.target.preventDefault();
+    library();
 }
-renderMarkUp.temp()
+renderMarkUp.temp();
--- src/js/pages/main.js
+++ src/js/pages/main.js
@@ -1,3 +1,36 @@
-export default function mainPage(){
-    
+import mainPageTemplate from '../../templates/mainPageTemplate.hbs';
+import movieListTemplate from '../../templates/filmCardsListTemplate.hbs';
+import fetchMethods from '../Api/fetchMethods';
+
+const refs = {
+    rootElement: document.querySelector('#root'),
+    filmList: document.querySelector('.films-library__gallery'),
+};
+
+function renderBaseMainPageMarkup() {
+    const markup = mainPageTemplate();
+    refs.rootElement.innerHTML = markup;
 }
+
+function renderMovieListItems(data) {
+    const firstPartOfMarkup = data.slice(0, 6);
+    const secondPartOfMarkup = data.slice(6, 12);
+    const thirdPartOfMarkup = data.slice(12);
+
+    const markup =
+        movieListTemplate(firstPartOfMarkup) +
+        movieListTemplate(secondPartOfMarkup) +
+        movieListTemplate(thirdPartOfMarkup);
+
+    refs.filmList.insertAdjacentHTML('beforeend', markup);
+}
+
+async function mainPage() {
+    renderBaseMainPageMarkup();
+
+    const filmsData = await fetchMethods.popularFilms();
+
+    renderMovieListItems(filmsData);
+}
+
+export default mainPage;
