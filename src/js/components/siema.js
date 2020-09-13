b14c77aa5dc5ca63e5cc7debfaaeaf1e681b274f
Slavik Ohorodnik <teatrgsc@gmail.com>
2020-09-13 18:15:43 +0300

    add siema on gallery

--- src/js/components/siema.js
+++ src/js/components/siema.js
@@ -1,19 +1,1 @@
-import Siema from 'siema';
-
-
-    const siema = new Siema({
-        selector: '.siema',
-        duration: 200,
-        easing: 'ease-out',
-        perPage: 1,
-        startIndex: 0,
-        draggable: true,
-        multipleDrag: true,
-        threshold: 20,
-        loop: false,
-        rtl: false,
-        onInit: () => {},
-        onChange: () => {},
-      })
-
 
--- src/js/pages/main.js
+++ src/js/pages/main.js
@@ -1,6 +1,9 @@
 import renderMarkUp from '../components/renderMarkUp';
 import fetchMethods from '../Api/fetchMethods';
 import searchListener from './search';
+import Siema from 'siema';
+
+
 
 export default async function mainPage() {
     renderMarkUp.mainPageCascade();
@@ -12,4 +15,20 @@ export default async function mainPage() {
         throw console.log(error);
     }
     renderMarkUp.popularMovies(popularMoviesResult);
+    
+    const siema = new Siema({
+        selector: '.siema',
+        duration: 200,
+        easing: 'ease-out',
+        perPage: 1,
+        startIndex: 0,
+        draggable: true,
+        multipleDrag: true,
+        threshold: 20,
+        loop: false,
+        rtl: false,
+        onInit: () => {},
+        onChange: () => {},
+      })
+
 }
