8ec8c72e3ed908b682da145107aae475adf839ff
Valerii Troian <trojan.valerii@gmail.com>
2020-09-13 22:21:22 +0300

    Aded clickHeandler to library.js and creat components/clickOnFilmCard.js

--- src/js/pages/library.js
+++ src/js/pages/library.js
@@ -4,10 +4,11 @@ import localStorage from '../components/localStorage';
 const QUEUE_KEY_IN_LS = 'filmsQueue';
 const WATCHED_KEY_IN_LS = 'filmsWatched';
 
-export default function library(){
+export default function library() {
     const data = getQueueFilmsFromLS();
     renderMarkUp.libraryPage(data);
-    console.log(data);
+    // нужно добавить проверку на длину масива в renderMarkUp.libraryPage, если длина меньше 0, то выводить для пользователя уведомление
+    onBntLibraryClick();
 }
 
 function getWachedFilmsFromLS() {
@@ -18,4 +19,31 @@ function getWachedFilmsFromLS() {
 function getQueueFilmsFromLS() {
     const data = localStorage.getFromLS(QUEUE_KEY_IN_LS);
     return data;
+}
+
+function onBntLibraryClick() {
+    const refs = {
+        btnQueue: document.querySelector('#js-btn-queue'),
+        btnWached: document.querySelector('#js-btn-watched'),
+    };
+
+    refs.btnWached.addEventListener('click', onWachedBtnClickHandler);
+    refs.btnQueue.addEventListener('click', onQueueBtnClickHandler);
+}
+
+function onQueueBtnClickHandler(event) {
+    event.preventDefault();
+    const data = getQueueFilmsFromLS();
+    onRenderMarkUpByBtn (data);
+}
+
+function onWachedBtnClickHandler(event) {
+    event.preventDefault();
+    const data = getWachedFilmsFromLS();
+    onRenderMarkUpByBtn (data);
+}
+
+function onRenderMarkUpByBtn (data) {
+    renderMarkUp.libraryPage(data);
+    onBntLibraryClick();
 }
